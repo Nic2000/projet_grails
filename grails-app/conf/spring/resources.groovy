6f564720d749622e76cb2b6b530bc5916c407034
Nicole Raharifetra <52413205+Nic2000@users.noreply.github.com>
2022-02-13 11:17:42 +0100

    fix api

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,4 +1,4 @@
-import fr.itu.mbds.grails.UserPasswordEncoderListener
+import itu.mbds.grails.UserPasswordEncoderListener
 // Place your Spring DSL code here
 beans = {
     userPasswordEncoderListener(UserPasswordEncoderListener)
--- grails-app/init/projet_itu_grails/BootStrap.groovy
+++ grails-app/init/projet_itu_grails/BootStrap.groovy
@@ -1,10 +1,10 @@
 package projet_itu_grails
 
-import fr.itu.mbds.grails.Annonce
-import fr.itu.mbds.grails.Illustration
-import fr.itu.mbds.grails.Role
-import fr.itu.mbds.grails.User
-import fr.itu.mbds.grails.UserRole
+import itu.mbds.grails.Annonce
+import itu.mbds.grails.Illustration
+import itu.mbds.grails.Role
+import itu.mbds.grails.User
+import itu.mbds.grails.UserRole
 
 class BootStrap {
 
@@ -13,39 +13,16 @@ class BootStrap {
         def roleAdmin = new Role(authority: 'ROLE_ADMIN').save()
         def roleModo = new Role(authority: 'ROLE_MODO').save()
 
-        def userAdmin = new User(username: "admin", password: "admin").save()
-        def userModo = new User(username: "modo", password: "modo").save()
+        def userAdmin = new User(username: "Lecoincoin", password: "admin").save()
+        def userModo = new User(username: "Mathilde", password: "modo").save()
 
         UserRole.create(userAdmin, roleAdmin, true)
         UserRole.create(userModo, roleModo, true)
-
-        ["Alice", "Bob", "Charly", "David", "Etienne"].each {
-            String uName ->
-                def userInstance = new User(username: uName, password: "password")
-                (1..5).each {
-                    Integer annonceIdx ->
-                        def annonceInstance = new Annonce(
-                                title: "Title $uName $annonceIdx",
-                                description: "Description $uName $annonceIdx",
-                                price: 100 * annonceIdx,
-                                status: Boolean.TRUE)
-                        (1..3).each {
-                            Integer illustrationIdx ->
-                                annonceInstance.addToIllustrations(new Illustration(filename: "grails.svg"))
-                        }
-                        userInstance.addToAnnonces(annonceInstance)
-                }
-                userInstance.save(flush:true, failOnError: true)
-        }
-
-        assert User.count == 7
-        assert Role.count == 2
-        assert Annonce.count == 25
-        assert Illustration.count == 75
 
 
     }
     def destroy = {
 
     }
+
 }
